01c059a15a173d1efe43754ee3ed3cf9d061a7f0
tad1 <tadziobrzeski@gmail.com>
2021-12-08 23:54:29 +0100

    Dodaj pliki projektów.

---
+++ HetinProject.cpp
@@ -0,0 +1,12 @@
+﻿// HetinProject.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "HetinProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
